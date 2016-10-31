f3dc9601dd24c31fbd1fdeeaf8ac831e0804ff77
gTopa <gtopa@stuy.edu>
2016-10-31 15:23:01 -0400

    wrote size thing

--- stat.c
+++ stat.c
@@ -3,6 +3,19 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+
+void sizechange(int size){
+  if (size>1000){
+    printf("%d KB\n", size/1000.0);
+  }else if(size>1000000){
+    printf("%d MB\n", size/1000000.0);
+  }else if(size>1000000000){
+    printf("%d GB\n", size/1000000000.0);
+  }else{
+    printf("%d B\n", size);
+  }
+}
+
 int main(){
   struct stat n;
   stat("stat.c", &n);
